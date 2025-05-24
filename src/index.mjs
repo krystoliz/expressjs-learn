fae16ed822482498b5276e1fd7ae5e891c7b47cb
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-05-24 14:34:59 +0700

    feat: root endpoint and api/users endpoint

--- src/index.mjs
+++ src/index.mjs
@@ -4,7 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+
+app.get('/', (request, response) => {
+    response.status(201).send({msg: 'Hello World!'});
+})
+
+app.get('/api/users', (request, response) => {
+    response.send([
+        {id: 1, username: 'jhon', displayName: 'Jhon Doe'}, 
+        {id: 2, username: 'jane', displayName: 'Jane Doe'}]);
+})
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
